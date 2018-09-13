60cfdc23bdf04019fd96877413c27a8198492985
VictorMoi <43212771+VictorMoi@users.noreply.github.com>
2018-09-13 20:33:38 +0200

    le soir

--- main.py
+++ main.py
@@ -12,4 +12,45 @@ dossier="C:/Users/Victor/Documents/programmes/blemais/"
 
 # on charge les données
 
-maize = pd.read_table(dossier+"TrainingDataSet_Maize.txt")
+PDmaize = pd.read_table(dossier+"TrainingDataSet_Maize.txt")
+colL = list(PDmaize)
+col={}
+for val,key in enumerate(colL):
+	col[key]=val
+
+
+maize = np.array(maize)
+
+colL.index("Tx_1")
+col["Tx_1]
+
+
+# création de nouvelles variables
+
+###  Déficit hydrique
+RUM = 10
+
+maize['RU_1']=RUM
+
+for(i in 2:9){
+  colRU<-paste0("RU_",i)
+  colRU1<-paste0("RU_",i-1)
+  colPR<-paste0("PR_",i)
+  colETP<-paste0("ETP_",i)
+  colETR<-paste0("ETR_",i)
+  colDE<-paste0("DE_",i)
+  DE<-(maize[,colPR]-maize[,colETP])<0
+  maize[!DE,colRU]<-pmin(maize[!DE,colRU1]+maize[!DE,colPR]-maize[!DE,colETP],RUM)
+  maize[DE,colRU]<-pmax(0,maize[DE,colRU1] * exp((maize[DE,colPR]-maize[DE,colETP])/RUM))
+  maize[!DE,colETR]<-maize[!DE,colETP]
+  maize[DE,colETR]<-maize[DE,colRU1]-maize[DE,colRU]+maize[DE,colPR]
+  maize[,colDE]<-maize[,colETP]-maize[,colETR]
+}
+
+
+
+
+
+
+
+
