6fef0089b9f2624dab4871b7c614f234a957a82c
VictorMoi <43212771+VictorMoi@users.noreply.github.com>
2018-09-13 22:34:29 +0200

    code ok R2Py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 if os.name == 'posix':
     project_path = os.getcwd()
 else:
-    project_path = 'C:/Users/Victor/Document/programmes/Github/blemais'
+    project_path = 'C:/Users/Victor/Documents/programmes/Github/blemais'
 
     
 # we load data
@@ -24,9 +24,10 @@ maize, ind2name, name2ind = loadData("TrainingDataSet_Maize.txt")
 
 
 def addColumn(arr, ind2name, name2ind, name, column):
+    column=np.transpose(np.array([column]))
     ind2name.append(name)
-    name2ind[name] = len(ind2name)
-    arr = np.concatenate(arr, column, axis=1)
+    name2ind[name] = len(ind2name)-1
+    arr = np.concatenate((arr, column), axis=1)
     return arr, ind2name, name2ind
 
 
@@ -41,26 +42,21 @@ RUM = 10
 
 
 
-maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, "RU_1", RUM)
+
+
+maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, "RU_1", np.array([RUM for i in range(len(maize))]))
 
 for i in range(2,10):
-    colRU = "RU_" + i
-    colRU1 = "RU_" + (i - 1)
-    colPR = "PR_" + i
-    colETP = "ETP_" + i
-    colETR = "ETR_" + i
-    colDE = "DE_" + i
+    colRU = "RU_" + str(i)
+    colRU1 = "RU_" + str(i - 1)
+    colPR = "PR_" + str(i)
+    colETP = "ETP_" + str(i)
+    colETR = "ETR_" + str(i)
+    colDE = "DE_" + str(i)
 
-    DE = (maize[name2ind[colPR]] - maize[name2ind[colETP]]) < 0
+    DE = (maize[:,name2ind[colPR]] - maize[:,name2ind[colETP]]) < 0
 
-    maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, colRU, np.minimum(maize[name2ind[colRU1]] + maize[name2ind[colPR]] - maize[name2ind[colETP]],RUM)*(1 - DE) + DE*np.maximum(maize[name2ind[colRU1]]*np.exp((maize[name2ind[colPR]] - maize[name2ind[colETP]])/RUM),0))
-    maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, colETR, DE*(maize[name2ind[colRU1]] + maize[name2ind[colPR]] - maize[name2ind[colRU]]) + (1-DE)*(maize[name2ind[colETP]]))
-    maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, colDE, maize[name2ind[colETP]] - maize[name2ind[colETR]])
-
-
-
-
-
-
-
+    maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, colRU, np.minimum(maize[:,name2ind[colRU1]] + maize[:,name2ind[colPR]] - maize[:,name2ind[colETP]],RUM)*(1 - DE) + DE*np.maximum(maize[:,name2ind[colRU1]]*np.exp((maize[:,name2ind[colPR]] - maize[:,name2ind[colETP]])/RUM),0))
+    maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, colETR, DE*(maize[:,name2ind[colRU1]] + maize[:,name2ind[colPR]] - maize[:,name2ind[colRU]]) + (1-DE)*(maize[:,name2ind[colETP]]))
+    maize, ind2name, name2ind = addColumn(maize, ind2name, name2ind, colDE, maize[:,name2ind[colETP]] - maize[:,name2ind[colETR]])
 
