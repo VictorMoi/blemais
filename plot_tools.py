83014cd39733d4e7f0356b2b3f15510e19ba1b00
VictorMoi <43212771+VictorMoi@users.noreply.github.com>
2018-10-25 22:17:13 +0200

    squared

--- plot_tools.py
+++ plot_tools.py
@@ -5,9 +5,15 @@ import matplotlib.pyplot as plt
 
 split_func = split_func_for_reg(year)
 
-
+x_tr, x_te, y_tr, y_te = split_func(x, y, test_size=0.1, random_state=0)
+x_tr, x_te, y_tr, y_te = split_func(x, y, test_size=0.1, random_state=1)
+x_tr, x_te, y_tr, y_te = split_func(x, y, test_size=0.1, random_state=2)
+x_tr, x_te, y_tr, y_te = split_func(x, y, test_size=0.1, random_state=3)
+x_tr, x_te, y_tr, y_te = split_func(x, y, test_size=0.1, random_state=4)
 x_tr, x_te, y_tr, y_te = split_func(x, y, test_size=0.1, random_state=5)
 
+
+x_te.shape
 
 yp = err[0]['reg'][1].predict(x_tr)
 plt.plot([-4,4], [-4,4], 'r')
