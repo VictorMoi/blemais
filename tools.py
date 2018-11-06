599a94be5544d421ac7401b658fd833a72d9070a
VictorMoi <43212771+VictorMoi@users.noreply.github.com>
2018-11-06 22:51:20 +0100

    del tools

--- tools.py
+++ tools.py
@@ -118,9 +118,6 @@ def splitTestYear(x, y, year, nb_year=4, seed=0, n=0):
 
 
 
-def aggregateData(x, y, year, var):
-    
-
 class split_func_for_reg:
     def __init__(self, year):
         self.year = year
